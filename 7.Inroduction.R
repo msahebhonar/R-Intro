fe8397ff370181341576f981b1034b0c35a3500c
Mohammad Sahebhonar <mohammad_sahebhonar@hotmail.com>
2016-04-28 22:43:41 +0430

    added by M. Sahebhonar

---
+++ 7.Inroduction.R
@@ -0,0 +1,28 @@
+# R is a programming language for statistical computing and data mining
+# R was developed by Ross Ihaka & Robert Gentelman 
+
+
+citation()
+?read.csv
+?"%%"
+help.search("fitting linear model")
+help.start()
+example(lm)
+data("airquality")
+View(airquality)
+
+data(lung)
+library(survival)
+data("lung")
+lung$surv <- Surv(lung$status)
+
+head(lung)
+tail(lung)
+
+#Write the comment
+2 + 3 ; round(0.018, 2)
+
+sqrt(627
+     )
+exp(9
+    )
---
+++ 8.Variable.R
@@ -0,0 +1,40 @@
+sqrt(9) + sqrt(16)
+(sqrt(9) + sqrt(16)) / 2
+
+x <- 3.5
+b <- 5
+b <- 6
+B <- 10
+
+1z <- 9
+%p <- 1
+
+age male <- 57
+age.male <- 57
+age_male <- 57
+
+y <- 25.5
+y
+class(y)
+
+t <- 1
+t
+class(t)
+
+i <- 3L
+i
+class(i)
+
+u <- 1
+v <- 2
+w <- u > v
+w 
+class(w)
+
+name <- "Mohammad"
+name
+class(name)
+
+z <- 3.8 + 2i
+z
+class(z)
